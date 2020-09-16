4e1d317dcef8ba89de53267961ed2f286da1cd6f
Gerben <gerben@treora.com>
2020-09-16 15:03:00 +0200

    Support matching across chunks

--- packages/dom/src/text-quote/match.ts
+++ packages/dom/src/text-quote/match.ts
@@ -54,36 +54,83 @@ interface AbstractRange<TChunk> {
 export function abstractTextQuoteSelectorMatcher(
   selector: TextQuoteSelector,
 ): <TChunk extends Chunk>(textChunks: AsyncIterable<TChunk>) => AsyncGenerator<AbstractRange<TChunk>, void, void> {
-  return async function* matchAll(textChunks) {
+  return async function* matchAll<TChunk extends Chunk>(textChunks: AsyncIterable<TChunk>) {
     const exact = selector.exact;
     const prefix = selector.prefix || '';
     const suffix = selector.suffix || '';
     const searchPattern = prefix + exact + suffix;
 
+    let partialMatches: Array<{
+      startChunk: TChunk;
+      startIndex: number;
+      charactersMatched: number;
+    }> = [];
+
     for await (const chunk of textChunks) {
       const chunkValue = chunk.toString();
 
-      // Find the pattern in the chunk (possibly multiple times)
-      // TODO allow pattern to be spread across chunks
-      let fromIndex = 0;
-      while (fromIndex <= chunkValue.length) {
-        const patternStartIndex = chunkValue.indexOf(searchPattern, fromIndex);
-        if (patternStartIndex === -1) break;
-
-        // Correct for the prefix and suffix lengths.
-        const matchStartIndex = patternStartIndex + prefix.length;
-        const matchEndIndex = matchStartIndex + exact.length;
-
-        yield {
-          startChunk: chunk,
-          startIndex: matchStartIndex,
-          endChunk: chunk,
-          endIndex: matchEndIndex,
-        };
-
-        // Advance the search forward to detect multiple occurrences within the same chunk.
-        fromIndex = matchStartIndex + 1;
+      // Continue checking any partial matches from the previous chunk(s).
+      const remainingPartialMatches: typeof partialMatches = [];
+      for (const { startChunk, startIndex, charactersMatched } of partialMatches) {
+        if (searchPattern.length - charactersMatched > chunkValue.length) {
+          if (chunkValue === searchPattern.substring(charactersMatched, charactersMatched + chunkValue.length)) {
+            // The chunk is too short to complete the match; comparison has to be completed in subsequent chunks.
+            remainingPartialMatches.push({
+              startChunk,
+              startIndex,
+              charactersMatched: charactersMatched + chunkValue.length,
+            });
+          }
+        }
+        else if (chunkValue.startsWith(searchPattern.substring(charactersMatched))) {
+          yield {
+            startChunk,
+            startIndex,
+            endChunk: chunk,
+            endIndex: searchPattern.length - charactersMatched,
+          };
+        }
       }
+      partialMatches = remainingPartialMatches;
+
+      // Try find the whole pattern in the chunk (possibly multiple times).
+      if (searchPattern.length <= chunkValue.length) {
+        let fromIndex = 0;
+        while (fromIndex <= chunkValue.length) {
+          const patternStartIndex = chunkValue.indexOf(searchPattern, fromIndex);
+          if (patternStartIndex === -1) break;
+
+          // Correct for the prefix and suffix lengths.
+          const matchStartIndex = patternStartIndex + prefix.length;
+          const matchEndIndex = matchStartIndex + exact.length;
+
+          yield {
+            startChunk: chunk,
+            startIndex: matchStartIndex,
+            endChunk: chunk,
+            endIndex: matchEndIndex,
+          };
+
+          // Advance the search forward to detect multiple occurrences within the same chunk.
+          fromIndex = matchStartIndex + 1;
+        }
+      }
+
+      // Check if this chunk ends with a partial match (or even multiple partial matches).
+      let newPartialMatches: number[] = [];
+      const searchStartPoint = Math.max(chunkValue.length - searchPattern.length + 1, 0);
+      for (let i = searchStartPoint; i < chunkValue.length; i++) {
+        const character = chunkValue[i];
+        newPartialMatches = newPartialMatches.filter(
+          partialMatchStartIndex => (character === searchPattern[i - partialMatchStartIndex])
+        );
+        if (character === searchPattern[0]) newPartialMatches.push(i);
+      }
+      newPartialMatches.forEach(partialMatchStartIndex => partialMatches.push({
+        startChunk: chunk,
+        startIndex: partialMatchStartIndex,
+        charactersMatched: chunkValue.length - partialMatchStartIndex,
+      }));
     }
   };
 }
