302ef071d1ba19803ddec2f336358ba1d4473c00
Gerben <gerben@treora.com>
2020-10-09 21:18:46 +0200

    Add semicolons etc

--- packages/dom/src/seek.ts
+++ packages/dom/src/seek.ts
@@ -1,89 +1,86 @@
-const E_END = 'Iterator exhausted before seek ended.'
-const E_SHOW = 'Argument 1 of seek must use filter NodeFilter.SHOW_TEXT.'
-const E_WHERE = 'Argument 2 of seek must be an integer or a Text Node.'
+const E_END = 'Iterator exhausted before seek ended.';
+const E_SHOW = 'Argument 1 of seek must use filter NodeFilter.SHOW_TEXT.';
+const E_WHERE = 'Argument 2 of seek must be an integer or a Text Node.';
 
-const DOCUMENT_POSITION_PRECEDING = 2
-const SHOW_TEXT = 4
-const TEXT_NODE = 3
-
+const DOCUMENT_POSITION_PRECEDING = 2;
+const SHOW_TEXT = 4;
+const TEXT_NODE = 3;
 
 export default function seek(iter: NodeIterator, where: number | Text): number {
   if (iter.whatToShow !== SHOW_TEXT) {
-    let error: Error & { code?: number }
+    let error: Error & { code?: number };
 
     // istanbul ignore next
     try {
-      error = new DOMException(E_SHOW, 'InvalidStateError')
+      error = new DOMException(E_SHOW, 'InvalidStateError');
     } catch {
       error = new Error(E_SHOW);
-      error.code = 11
-      error.name = 'InvalidStateError'
-      error.toString = () => `InvalidStateError: ${E_SHOW}`
+      error.code = 11;
+      error.name = 'InvalidStateError';
+      error.toString = () => `InvalidStateError: ${E_SHOW}`;
     }
 
-    throw error
+    throw error;
   }
 
-  let count = 0
-  let node: Node | null = iter.referenceNode
-  let predicates = null
+  let count = 0;
+  let node: Node | null = iter.referenceNode;
+  let predicates = null;
 
   if (isInteger(where)) {
     predicates = {
       forward: () => count < where,
       backward: () => count > where || !iter.pointerBeforeReferenceNode,
-    }
+    };
   } else if (isText(where)) {
-    let forward = before(node, where) ? () => false : () => node !== where
-    let backward = () => node !== where || !iter.pointerBeforeReferenceNode
-    predicates = {forward, backward}
+    predicates = {
+      forward: before(node, where) ? () => false : () => node !== where,
+      backward: () => node !== where || !iter.pointerBeforeReferenceNode,
+    };
   } else {
-    throw new TypeError(E_WHERE)
+    throw new TypeError(E_WHERE);
   }
 
   while (predicates.forward()) {
-    node = iter.nextNode()
+    node = iter.nextNode();
 
     if (node === null) {
-      throw new RangeError(E_END)
+      throw new RangeError(E_END);
     }
 
-    count += (node as Text).data.length
+    count += (node as Text).data.length;
   }
 
   if (iter.nextNode()) {
-    node = iter.previousNode()
+    node = iter.previousNode();
   }
 
   while (predicates.backward()) {
-    node = iter.previousNode()
+    node = iter.previousNode();
 
     if (node === null) {
-      throw new RangeError(E_END)
+      throw new RangeError(E_END);
     }
 
-    count -= (node as Text).data.length
+    count -= (node as Text).data.length;
   }
 
   if (!isText(iter.referenceNode)) {
     throw new RangeError(E_END);
   }
 
-  return count
+  return count;
 }
-
 
 function isInteger(n: any): n is number {
   if (typeof n !== 'number') return false;
   return isFinite(n) && Math.floor(n) === n;
 }
 
-
 function isText(node: Node): node is Text {
-  return node.nodeType === TEXT_NODE
+  return node.nodeType === TEXT_NODE;
 }
 
-
 function before(ref: Node, node: Node): boolean {
-  return !!(ref.compareDocumentPosition(node) & DOCUMENT_POSITION_PRECEDING)
+  return !!(ref.compareDocumentPosition(node) & DOCUMENT_POSITION_PRECEDING);
 }
